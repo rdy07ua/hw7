28ff146f37cdef1ebec8fadbcf55c904894a84bf
rdy07ua <spartakadoma.net@gmail.com>
2023-07-03 10:26:20 +0200

    added

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,58 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+//Створити метод який повертає куб заданого числа
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static int cube(int number) {
+        return number * number * number;
+    } // Куб числа
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void printStars(int countStars) {
+        for (int i = 0; i < countStars; i++) {
+            System.out.print("*");    /////////////////////////////////////виводимо зірочки
         }
     }
+
+    public static void printSymbols(char symbol, int countOfSymbols){
+    for (int i = 0; i  < countOfSymbols; i++) {
+        System.out.print(symbol);   //////////////////////////////////////виводимо символи
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int number = scanner.nextInt();
+        int cubeResult = cube(number);
+        System.out.println("the cube of your number is " + cubeResult);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        System.out.println("Enter the count of Stars");
+        int countStars = scanner.nextInt();
+        printStars(countStars);
+
+        System.out.println("\n");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        System.out.println("Enter the symbol");
+        char symbol = scanner.next().charAt(0);
+        System.out.println("Enter the count of symbols");
+        int countOfSymbols = scanner.nextInt();
+        printSymbols(symbol, countOfSymbols);
+
 }
+
+
+    }
+
+
+
+
+
+
